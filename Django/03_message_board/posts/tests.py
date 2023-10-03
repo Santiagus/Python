9a7ff2ab19c5dd8ecb99060eff549e05f4fec2ce
Santi <santiagoabad@gmail.com>
2023-10-03 14:08:54 +0200

    Add initial test

--- Django/03_message_board/posts/tests.py
+++ Django/03_message_board/posts/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Post
+
+
+class PostTest(TestCase):
+    @classmethod
+    def setUpTestData(cls) -> None:
+        cls.post = Post.objects.create(text="This is a test!")
+
+    def test_model_content(self):
+        self.assertEqual(self.post.text, "This is a test!")
