095d8197180e8c0b54e50aea51fc65f03d9ec208
Santi <santiagoabad@gmail.com>
2023-10-05 20:15:00 +0200

    Add post edition feature

--- Django/04_blog/blog_app/views.py
+++ Django/04_blog/blog_app/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from .models import Post
 
 
@@ -16,3 +16,9 @@ class BlogCreateView(CreateView):
     model = Post
     template_name = "post_new.html"
     fields = ["title", "author", "body"]
+
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name = "post_edit.html"
+    fields = ["title", "body"]
