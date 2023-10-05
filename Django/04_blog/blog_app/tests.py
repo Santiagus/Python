b45a4cee2050ce88e95877bd0774c24b8825772d
Santi <santiagoabad@gmail.com>
2023-10-05 15:55:41 +0200

    Add views test

--- Django/04_blog/blog_app/tests.py
+++ Django/04_blog/blog_app/tests.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from django.urls import reverse
 
 # from django.contrib.auth import get_user_model
 from django.contrib.auth.models import User
@@ -39,3 +40,24 @@ class BlogTest(TestCase):
     def test_url_exists_at_correct_location_detailview(self):
         response = self.client.get("/post/1/")
         self.assertEqual(response.status_code, 200)
+
+    def test_post_listview(self):
+        response = self.client.get(reverse("home"))
+        with self.subTest(msg="post status code check"):
+            self.assertEqual(response.status_code, 200)
+        with self.subTest(msg="post content check"):
+            self.assertContains(response, "Nice body content")
+        with self.subTest(msg="post template used"):
+            self.assertTemplateUsed(response, "home.html")
+
+    def test_post_detailview(self):
+        response = self.client.get(reverse("post_detail", kwargs={"pk": self.post.pk}))
+        no_response = self.client.get("post/100000")
+        with self.subTest(msg="no response"):
+            self.assertEqual(no_response.status_code, 404)
+        with self.subTest(msg="post response"):
+            self.assertEqual(response.status_code, 200)
+        with self.subTest(msg="post content check"):
+            self.assertContains(response, "A good title")
+        with self.subTest(msg="post content check"):
+            self.assertTemplateUsed(response, "post_detail.html")
