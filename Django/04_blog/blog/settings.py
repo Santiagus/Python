5dcd7f0138eb5a23476d813e44356409fe330ec0
Santi <santiagoabad@gmail.com>
2023-10-06 12:10:01 +0200

    Add LogOut in base.html

--- Django/04_blog/blog/settings.py
+++ Django/04_blog/blog/settings.py
@@ -124,4 +124,5 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-LOGIN_REDIRECT_URL = "home"  # new
+LOGIN_REDIRECT_URL = "home"
+LOGOUT_REDIRECT_URL = "home"
