e63f2c9c1a909d9435758fb2f1cfc260a49cb106
Santi <santiagoabad@gmail.com>
2023-10-06 10:22:36 +0200

    Add create,update,delete post tests

--- Django/04_blog/blog_app/tests.py
+++ Django/04_blog/blog_app/tests.py
@@ -101,3 +101,35 @@ class BlogTest(TestCase):
             self.assertTemplateUsed(
                 response, template_check, msg_prefix=colored(template_msg, "red")
             )
+
+
+def test_post_createview(self):  # new
+    response = self.client.post(
+        reverse("post_new"),
+        {
+            "title": "New title",
+            "body": "New text",
+            "author": self.user.id,
+        },
+    )
+    self.assertEqual(response.status_code, 302)
+    self.assertEqual(Post.objects.last().title, "New title")
+    self.assertEqual(Post.objects.last().body, "New text")
+
+
+def test_post_updateview(self):  # new
+    response = self.client.post(
+        reverse("post_edit", args="1"),
+        {
+            "title": "Updated title",
+            "body": "Updated text",
+        },
+    )
+    self.assertEqual(response.status_code, 302)
+    self.assertEqual(Post.objects.last().title, "Updated title")
+    self.assertEqual(Post.objects.last().body, "Updated text")
+
+
+def test_post_deleteview(self):
+    response = self.client.post(reverse("post_delete", args="1"))
+    self.assertEqual(response.status_code, 302)
