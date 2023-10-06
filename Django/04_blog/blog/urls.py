e06ba43bf953ad72daefaee8b5209b8fc2613c53
Santi <santiagoabad@gmail.com>
2023-10-06 13:02:36 +0200

    Add SignUp feature

--- Django/04_blog/blog/urls.py
+++ Django/04_blog/blog/urls.py
@@ -17,8 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+# order matters, Django read it from top to bottom
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("django.contrib.auth.urls")),
+    path("accounts/", include("accounts.urls")),
     path("", include("blog_app.urls")),
 ]
