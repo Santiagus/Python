294aaad6c283f0b010883b7c2514a6b03a7a5f38
Santi <santiagoabad@gmail.com>
2023-10-08 20:55:08 +0200

    Add test pages homepage location check

--- Django/05_Newspaper/pages/tests.py
+++ Django/05_Newspaper/pages/tests.py
@@ -1,3 +1,10 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
+from django.urls import reverse
+from http import HTTPStatus
+
 
 # Create your tests here.
+class HomePageTest(SimpleTestCase):
+    def test_url_exists_at_correct_location_homepageview(self):
+        response = self.client.get("home.tml")
+        self.assertEqual(response.status_code, HTTPStatus.OK)
