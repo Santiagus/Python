43341ceaecfb036c9934b05730516a31408a9059
Santi <santiagoabad@gmail.com>
2023-10-08 19:39:32 +0200

    Add test signup view at correct location

--- Django/05_Newspaper/accounts/tests.py
+++ Django/05_Newspaper/accounts/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
 
 # Create your tests here.
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+
+class SingUpTetst(TestCase):
+    def test_url_exist_at_the_correct_location_signupview(self):
+        response = self.client.get("/accounts/signup/")
+        self.assertEqual(response.status_code, 200)
