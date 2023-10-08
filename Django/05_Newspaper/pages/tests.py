f2b760319a01dbf0dfba4a0b841dd3e5a6f68d41
Santi <santiagoabad@gmail.com>
2023-10-08 21:01:05 +0200

    Update test homepage view

--- Django/05_Newspaper/pages/tests.py
+++ Django/05_Newspaper/pages/tests.py
@@ -6,5 +6,14 @@ from http import HTTPStatus
 # Create your tests here.
 class HomePageTest(SimpleTestCase):
     def test_url_exists_at_correct_location_homepageview(self):
-        response = self.client.get("home.tml")
+        response = self.client.get("/")
         self.assertEqual(response.status_code, HTTPStatus.OK)
+
+    def test_homepage_view(self):
+        response = self.client.get(reverse("home"))
+        with self.subTest(msg="homepage name resolution check"):
+            self.assertEqual(response.status_code, HTTPStatus.OK)
+        with self.subTest(msg="homepage template used check"):
+            self.assertTemplateUsed(response, "home.html")
+        with self.subTest(msg="homepage contain check"):
+            self.assertContains(response, "Home")
