b8bda1b345b838f2384583789b24e66d60f7e5fc
Santi <santiagoabad@gmail.com>
2023-10-08 19:47:50 +0200

    Update Test signup view name, template

--- Django/05_Newspaper/accounts/tests.py
+++ Django/05_Newspaper/accounts/tests.py
@@ -9,3 +9,10 @@ class SingUpTetst(TestCase):
     def test_url_exist_at_the_correct_location_signupview(self):
         response = self.client.get("/accounts/signup/")
         self.assertEqual(response.status_code, 200)
+
+    def test_signup_view_name(self):
+        response = self.client.get(reverse("signup"))
+        with self.subTest(msg="signup name test"):
+            self.assertEqual(response.status_code, 200)
+        with self.subTest(msg="signup templated used"):
+            self.assertTemplateUsed(response, "registration/signup.html")
