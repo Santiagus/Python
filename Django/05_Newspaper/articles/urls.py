1626bf911204e9e4fa933de8169e1547808b6682
Santi <santiagoabad@gmail.com>
2023-10-10 10:54:44 +0200

    Fix ArticleUpdateView name

--- Django/05_Newspaper/articles/urls.py
+++ Django/05_Newspaper/articles/urls.py
@@ -8,7 +8,7 @@ from .views import (
 
 urlpatterns = [
     path("<int:pk>/", ArticleDetailView.as_view(), name="article_detail"),
-    path("<int:pk>/edit/", ArticleUpdateView.as_view(), name="article_update"),
+    path("<int:pk>/edit/", ArticleUpdateView.as_view(), name="article_edit"),
     path("<int:pk>/delete/", ArticleDeleteView.as_view(), name="article_delete"),
     path("", ArticleListView.as_view(), name="article_list"),
 ]
