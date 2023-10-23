1611ca56855c06afdef084c2d545c23146c0b26c
Santi <santiagoabad@gmail.com>
2023-10-23 18:54:51 +0200

    Add order's update datetime

--- APIs_and_Microservices/00_BasicFastAPI/orders/api/api.py
+++ APIs_and_Microservices/00_BasicFastAPI/orders/api/api.py
@@ -16,6 +16,7 @@ order = {
     "id": "ff0f1355-e821-4178-9567-550dec27a373",
     "status": "delivered",
     "created": datetime.utcnow(),
+    "updated": datetime.utcnow(),
     "order": [{"product": "cappuccino", "size": "medium", "quantity": 1}],
 }
 
