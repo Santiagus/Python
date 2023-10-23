58bfa0c0763d84d56f36098f1c6f13e9bbc3bb67
Santi <santiagoabad@gmail.com>
2023-10-23 12:01:21 +0200

    Fix typo

--- APIs_and_Microservices/00_BasicFastAPI/orders/api/api.py
+++ APIs_and_Microservices/00_BasicFastAPI/orders/api/api.py
@@ -22,7 +22,7 @@ order = {
 
 @app.get('/orders')
 def get_orders():
-    return {'orders': [orders]}
+    return {'orders': [order]}
 
 @app.post('/orders', status_code=status.HTTP_201_CREATED)
 def create_order():
