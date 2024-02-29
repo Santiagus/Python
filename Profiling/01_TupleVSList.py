8d93d616d9e98a7513624163f0221134b8067899
Santi Abad <santiagoabad@gmail.com>
2024-02-29 22:07:33 +0100

    feat: Add List Comprehension vs Generator expression

---
+++ Profiling/01_TupleVSList.py
@@ -0,0 +1,24 @@
+from performance_fixtures import time_measurer, memory_usage
+import asyncio
+
+
+@time_measurer
+@memory_usage
+async def search_unknown_tuple(haystack, needle):
+    return any((item == needle for item in haystack))
+
+
+@time_measurer
+@memory_usage
+async def search_unknown_list(haystack, needle):
+    return any([item == needle for item in haystack])
+
+
+print("Program start...")
+needle = 1000000
+haystack = (i for i in range(needle * 2))
+
+asyncio.run(search_unknown_tuple(haystack, needle))
+asyncio.run(search_unknown_list(haystack, needle))
+
+print("Program finished.")
