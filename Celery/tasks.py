eaef30fdb19a75532304b1d7178fb661ce8b8c54
Santiago Abad <santiagoabad@gmail.com>
2026-09-07 18:20:35 +0200

    feat: load config from configuration module

--- Celery/tasks.py
+++ Celery/tasks.py
@@ -1,14 +1,24 @@
 from celery import Celery
 
+
+"""
+ 1. Explicit config case
+"""
 # RabbitMQ broker URL using the pyamqp (librabbitmq/amqp) driver
-BROKER_URL = 'pyamqp://guest:guest@localhost:5672//'
+# BROKER_URL = 'pyamqp://guest:guest@localhost:5672//'
 
 # Celery RPC result backend (returns results via transient RabbitMQ reply queues)
 # Set to None if you do not need to retrieve return values with result.get()
-BACKEND_URL = 'rpc://'
+# BACKEND_URL = 'rpc://'
 
-app = Celery('tasks', broker=BROKER_URL, backend=BACKEND_URL)
+# app = Celery('tasks', broker=BROKER_URL, backend=BACKEND_URL)
 
+"""
+ 2.  Load config from configuration module ---
+"""
+
+app = Celery('tasks')
+app.config_from_object('celeryconfig')
 
 @app.task
 def add(x, y):
